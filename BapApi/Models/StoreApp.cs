418cc563de6d8b2cf3ee0cc52ec55835e3e41c8b
Simana Ale <50032557@belfastmet.ac.uk>
2021-07-02 16:41:39 +0100

    add DTO objects comments

--- BapApi/Models/StoreApp.cs
+++ BapApi/Models/StoreApp.cs
@@ -11,15 +11,42 @@ namespace BapApi.Models
        public string Price{ get; set; }
     }
 
+    /// <summary>
+    /// DATE: 02/07/2021
+    /// 
+    /// [1] Data transfer object (DTO)
+    /// To prevent our web API exposing the database entities to the client.
+    /// Such as the client receiving data that is mapping directly to our
+    /// database tables. Which is not a good idea thus we will create the StoreAppDTO.
+    /// Data Transfer Object (DTO) to send secured data over the network.
+    /// 
+    /// In programming the data transfer object (DTO) is an object 
+    /// that carries data between processes. The motivation for its use is that 
+    /// communication between processes is usually done resorting to remote interfaces 
+    /// (e.g., web services), where each call is an expensive operation
+    /// because the majority of the cost of each call is related to the round-trip 
+    /// time between the client and the server, one way of reducing the number of calls 
+    /// is to use an object (the DTO) that aggregates the data that would have been 
+    /// transferred by the several calls, but that is served by one call only.
+    /// 
+    /// The difference between data transfer objects and business objects or data 
+    /// access objects is that a DTO does not have any behavior except for storage, 
+    /// retrieval, serialization and deserialization of its own data(mutators, accessors,
+    /// parsers and serializers). In other words, DTOs are simple objects that should not
+    /// contain any business logic but may contain serialization and deserialization 
+    /// mechanisms for transferring data over the wire. 
+    /// https://en.wikipedia.org/wiki/Data_transfer_object
+    /// https://docs.microsoft.com/en-us/aspnet/web-api/overview/data/using-web-api-with-entity-framework/part-5
+    /// </summary>
     public class StoreAppDTO
     {
-       public int Id { get; set; }
-        public string Name { get; set; }
-        public double Rating { get; set; }
-        public int People { get; set; }
-        public string Category { get; set; }
-        public string Date { get; set; }
-        public string Price { get; set; }
+        public int     Id        { get; set; }
+        public string  Name      { get; set; }
+        public double  Rating    { get; set; }
+        public int     People    { get; set; }
+        public string  Category  { get; set; }
+        public string  Date      { get; set; }
+        public string  Price     { get; set; }
 
     }
 
