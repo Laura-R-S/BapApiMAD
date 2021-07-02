a0d350899808a9e9fae0b669d010e48048afde7f
Simana Ale <50032557@belfastmet.ac.uk>
2021-07-02 17:08:46 +0100

    update api people

--- BapApi/Models/StoreApp.cs
+++ BapApi/Models/StoreApp.cs
@@ -27,7 +27,12 @@ namespace BapApi.Models
        public double   Rating    { get; set; }
 
 
+       [Column("people")]
+       [Display(Name = "Number of People Downloaded")]
+       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
        public int      People    { get; set; }
+
+
        public string   Category  { get; set; }
        public string   Date      { get; set; }
        public string   Price     { get; set; }
